a7d4de238af969e5abdd70212e84435b0220e2b9
jwhuh <rhdfl7@gmail.com>
2024-11-22 21:16:30 +0900

    push get it

--- pages/stores/[storeId]/reviews/index.vue
+++ pages/stores/[storeId]/reviews/index.vue
@@ -227,7 +227,7 @@
 
         <!-- 도움돼요 버튼 -->
         <button
-          class="flex items-center space-x-2 border border-gray-300 px-3 py-2 rounded-full"
+          class="flex items-center space-x-2 border border-gray-300 mt-3 px-3 py-2 rounded-full"
           :class="{
             'bg-gradient-to-r from-rose-500 to-rose-600 text-white':
               review.liked,
